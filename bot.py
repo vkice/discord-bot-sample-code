ebadf0742c56e93ee22956e09c5a0759a2f6b7c2
vkiceina <50276044+vkice@users.noreply.github.com>
2023-03-13 21:42:39 -0400

    Modified Python code for clarity

--- bot.py
+++ bot.py
@@ -6,10 +6,6 @@ load_dotenv()
 
 bot = discord.Bot(intents=discord.Intents.all())
 
-@bot.event
-async def on_member_join(member):
-    await member.send(f'Welcome, {member.mention}!')
-
 @bot.slash_command(name="hello",str=None)
 async def hello(ctx):
     name = ctx.author.name
